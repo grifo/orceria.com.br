d3d33366f58bd897ee290b397fdedac7721369f5
Daniel Martins <danielpmartins@gmail.com>
2012-11-19 17:19:51 -0200

    if email is empty do nothing

--- catapult/request.php
+++ catapult/request.php
@@ -5,30 +5,6 @@
 	$password = "";
 	$database = "";
 	$table = "";
-	$isAjax = false;
-
-	if (
-		isset($_SERVER['HTTP_X_REQUESTED_WITH']) 
-		&& !empty($_SERVER['HTTP_X_REQUESTED_WITH']) 
-		&& strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) == 'xmlhttprequest'
-	) 
-	{
-		$isAjax = true;
-	}
-	echo 'Is Ajax? ';
-	echo $isAjax ? 'sim' : 'não' ;
-	echo '<br>';
-
-
-	$con = mysql_connect($host, $user, $password);
-	mysql_set_charset("UTF8", $con);
-
-	if (!$con)
-	{
-		die('Could not connect: ' . mysql_error());
-	}
-
-	mysql_select_db($database, $con);
 
 	//busca os dados do form, alguns podem ser nulos
 	$email = $_POST['email'];
@@ -40,17 +16,6 @@
 	$personOrEntity = !empty($_POST['personorentity']) ? $_POST['personorentity'] : null;
 	$suggestions = !empty($_POST['suggestions']) ? $_POST['suggestions'] : null;
 
-	echo "Buscando dados do form <br>";
-	echo $email; echo "<br>";
-	echo $name; echo "<br>";
-	echo $phone; echo "<br>";
-	echo $perfil; echo "<br>";
-	echo $field; echo "<br>";
-	echo $expectations; echo "<br>";
-	echo $personOrEntity; echo "<br>";
-	echo $suggestions; echo "<br>";
-	echo "<br>";
-
 	$email = mysql_real_escape_string($email);
 	$name = mysql_real_escape_string($name);
 	$phone = mysql_real_escape_string($phone);
@@ -60,16 +25,20 @@
 	$personOrEntity = mysql_real_escape_string($personOrEntity);
 	$suggestions = mysql_real_escape_string($suggestions);
 
-	echo "Buscando dados do form REAL ESCAPE<br>";
-	echo $email; echo "<br>";
-	echo $name; echo "<br>";
-	echo $phone; echo "<br>";
-	echo $perfil; echo "<br>";
-	echo $field; echo "<br>";
-	echo $expectations; echo "<br>";
-	echo $personOrEntity; echo "<br>";
-	echo $suggestions; echo "<br>";
-	echo "<br>";
+	//se o email for vazio faz mais nada
+	if (empty($email))
+		return;
+
+	//inicia a conexão no banco
+	$con = mysql_connect($host, $user, $password);
+	mysql_set_charset("UTF8", $con);
+
+	if (!$con)
+	{
+		die('Could not connect: ' . mysql_error());
+	}
+
+	mysql_select_db($database, $con);
 
 	//busca os dados atuais e sobreescreve se estiverem no form
 	$result = mysql_query("SELECT * FROM `$table` WHERE email = '$email'");
@@ -87,18 +56,6 @@
 		$suggestions = !empty($suggestions) ? $suggestions : $row['suggestions'];
 	}
 
-	echo "Atualizando com os dados do banco <br>";
-	echo $jaExiste."<br>";
-	echo $email."<br>";
-	echo $name."<br>";
-	echo $phone."<br>";
-	echo $perfil."<br>";
-	echo $field."<br>";
-	echo $expectations."<br>";
-	echo $personOrEntity."<br>";
-	echo $suggestions."<br>";
-	echo "<br>";
-
 	if ($jaExiste)
 	{
 		$sql = "UPDATE `$table`
@@ -110,10 +67,6 @@
 		$sql = "INSERT INTO `$table` (email, name, personorentity, phone, perfil, field, expectations, suggestions, date) 
 			VALUES ('$email', '$name', '$personOrEntity', '$phone', '$perfil', '$field', '$expectations', '$suggestions', NOW())";
 	}
-
-	echo "sql:<br>";
-	echo $sql;
-	echo "<br>";
 
 	if (!mysql_query($sql, $con))
 	{
